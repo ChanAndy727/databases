4868a4c908e8947ef3ec48f2319d2f1ec7a9833f
Andy Chan <Chanandy727@gmail.com>
2022-03-16 16:39:32 -0700

    complete designing multi-table schema

--- server/schema.sql
+++ server/schema.sql
@@ -4,7 +4,19 @@ USE chat;
 
 CREATE TABLE messages (
   /* Describe your table here.*/
+  ID int,
+  Username char(20),
+  content char(200),
+  roomname char(20),
+  PRIMARY KEY (ID)
 );
+
+CREATE TABLE users (
+  ID int,
+  Username char(20),
+  PRIMARY KEY (ID)
+);
+
 
 /* Create other tables and define schemas for them here! */
 
